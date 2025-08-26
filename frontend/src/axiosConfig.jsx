edbd3764c57371a7ca4091d9283ddfa13581e27e
alwints12-source <alwints12@gmail.com>
2025-08-26 16:24:09 +1000

    ec2 public ip

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://3.106.231.163:5001/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://16.176.9.170:5001/api',
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true,
 });
